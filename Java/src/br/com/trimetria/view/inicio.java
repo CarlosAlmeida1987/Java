9adfb98b21ddc9500659d3c4f967d38aac9fbc6a
CarlosAlmeida1987 <trimetria@hotmail.com>
2020-03-19 15:42:17 -0300

    Inicio 2

---
+++ Java/src/br/com/trimetria/view/inicio.java
@@ -0,0 +1,19 @@
+package br.com.trimetria.view;
+
+public class inicio {
+	
+	private String modelo;
+	
+	public inicio(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+}
