e9bbcb1f75215ae995bb1e9bf0392df98ade7984
CarlosAlmeida1987 <trimetria@hotmail.com>
2020-03-19 15:22:40 -0300

    add java projet
    add java projet

---
+++ Java/src/br/com/trimetria/Java.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package br.com.trimetria;
+
+/**
+ * @author Carlos
+ *
+ */
+public class Java {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
